64b9c5f9c932d227a8ca1bcc756194fb725a1eff
lixinyang123 <lixinyangemil@outlook.com>
2023-02-01 18:10:29 +0800

    change window position

--- Recoder/App.axaml.cs
+++ Recoder/App.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Recoder.ViewModels;
 using Recoder.Views;
+using System;
 
 namespace Recoder
 {
@@ -17,10 +18,20 @@ namespace Recoder
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow
+                MainWindow window = new()
                 {
-                    DataContext = new MainWindowViewModel(),
+                    DataContext = new MainWindowViewModel()
                 };
+
+                if (window.Screens.Primary is null)
+                {
+                    throw new PlatformNotSupportedException();
+                }
+
+                int positionX = Convert.ToInt32(window.Screens.Primary.Bounds.Width / 2 - window.Width / 2);
+                window.Position = new PixelPoint(positionX, 0);
+
+                desktop.MainWindow = window;
             }
 
             base.OnFrameworkInitializationCompleted();
--- Recoder/Views/MainWindow.axaml.cs
+++ Recoder/Views/MainWindow.axaml.cs
@@ -1,5 +1,4 @@
 using Avalonia.Controls;
-using System;
 
 namespace Recoder.Views
 {
@@ -8,14 +7,6 @@ namespace Recoder.Views
         public MainWindow()
         {
             InitializeComponent();
-
-            if (Screens.Primary is null)
-            {
-                throw new PlatformNotSupportedException();
-            }
-
-            int positionX = Convert.ToInt32(Screens.Primary.Bounds.Width / 2 - Width / 2);
-            Position = new Avalonia.PixelPoint(positionX, 0);
         }
     }
 }
