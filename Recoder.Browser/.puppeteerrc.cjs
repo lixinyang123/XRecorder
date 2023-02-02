6fddaa92e22b8bab769d57402404584bcf755708
lixinyang123 <lixinyangemil@outlook.com>
2023-02-03 01:34:07 +0800

    change proj name

--- Recoder.Browser/.puppeteerrc.cjs
+++
@@ -1,8 +0,0 @@
-const {join} = require('path');
-
-module.exports = {
-	cacheDirectory: join(__dirname, './'),
-	experiments: {
-		macArmChromiumEnabled: true,
-	}
-};
--- Recoder.Browser/README.md
+++
@@ -1,3 +0,0 @@
-# Recoder.Browser
-
-
