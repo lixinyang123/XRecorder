c4e1af1e2fd39e2690e386f8f81d845e1101b03b
lixinyang123 <lixinyangemil@outlook.com>
2023-02-07 02:51:41 +0800

    add build script

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,46 @@
-dotnet publish -r win-x64 -c Release --self-contained
-makensis publish.nsi
+Write-Output "===================================="
+Write-Output "|         Recorder Builder         |"
+Write-Output "===================================="
+
+$Platform = "UnSupport"
+
+if($IsWindows) {
+    $Platform = "win-x64"
+}
+
+if ($IsLinux) {
+    $Platform = "linux-x64"
+}
+
+if($IsMacOS) {
+    $Platform = "osx-x64"
+}
+
+if ("UnSupport" -eq $Platform) {
+    Throw "UnSupport Platform"
+}
+
+$LibFolder = "Recorder/lib"
+$PublishFolder = "Recorder/bin/Release/net7.0/$Platform/publish"
+
+if (Test-Path $PublishFolder) {
+    Remove-Item $PublishFolder -Recurse
+}
+
+Copy-Item -Path $LibFolder -Destination $PublishFolder -Recurse
+
+dotnet publish -r $Platform -c Release --self-contained
+
+# ====================== Build Setup App ======================
+
+if($IsWindows) {
+    makensis installer.nsi
+}
+
+if ($IsLinux) {
+    # build appimage
+}
+
+if($IsMacOS) {
+    # build dmg
+}
