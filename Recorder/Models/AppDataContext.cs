f1903146bc4b87e2472cadc4c406482805d89ef9
lixinyang123 <lixinyangemil@outlook.com>
2023-02-07 13:47:25 +0800

    fixed the path error on unix

--- Recorder/Models/AppDataContext.cs
+++ Recorder/Models/AppDataContext.cs
@@ -1,4 +1,5 @@
 using ReactiveUI;
+using System;
 using System.IO;
 
 namespace Recorder.Models
@@ -13,7 +14,15 @@ namespace Recorder.Models
         {
             // Get assembly path
             string hostName = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            appPath = hostName.Substring(0, hostName.LastIndexOf("\\"));
+
+            if (OperatingSystem.IsWindows())
+            {
+                appPath = hostName.Substring(0, hostName.LastIndexOf("\\"));
+            }
+            else
+            {
+                appPath = hostName.Substring(0, hostName.LastIndexOf("/"));
+            }
             
             captureResources = Path.Combine(appPath, "CaptureResources");
         }
